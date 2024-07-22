4153f8df6f50ee91225b17e4c7e405c07cb46891
عاطفه قاسمی <a.ghasemi@ArianSystemdp.local>
2024-07-22 13:15:26 +0330

    first deploy

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <p>هین یک پروژه تست هست و من عاطفه قاسمی هستم</p>
+    </main>
+  );
+}
